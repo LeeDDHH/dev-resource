8958734b0d33694ae9c00bd9646f04291db3dfb8
DongHeon Lee <niche3600@gmail.com>
2021-03-23 12:15:29 +0900

    react-gaを使わず、gtagでPVを取得

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,26 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import { ApolloProvider } from "@apollo/client";
 import client from "../lib/apollo-client";
+
+import { pageview } from '../lib/gtag'
 
 import '../styles/app.css'
 
 const Provider = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      pageview(url)
+      if (!shallow) { gtag.pageview(url) }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
