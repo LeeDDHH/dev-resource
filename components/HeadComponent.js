f89170b326700ffd9970660dfb3279af7b7f4c23
DongHeon Lee <niche3600@gmail.com>
2021-03-25 07:57:43 +0900

    next-seoでSEO対策

--- components/HeadComponent.js
+++ components/HeadComponent.js
@@ -1,10 +1,41 @@
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 const HeadComponent = () => {
   return (
-    <Head>
-      <title>Resource-of-Develop</title>
-    </Head>
+    <NextSeo
+      title="Resource of Develop"
+      description="開発やプログラミング勉強に役立つリソースが見つかるサービス"
+      canonical="https://rod.expfrom.me/"
+      openGraph={{
+        url: 'https://rod.expfrom.me/',
+        title: 'Resource of Develop',
+        description: '開発やプログラミング勉強に役立つリソースが見つかるサービス',
+        images: [
+          { url: '/images/og/ogp_image.png' }
+        ],
+        site_name: 'Resource of Develop'
+      }}
+      twitter={{
+        handle: '@camomile_cafe',
+        site: '@camomile_cafe',
+        cardType: 'summary_large_image'
+      }}
+      additionalLinkTags={[
+        {
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.jpg',
+          sizes: '180x180'
+        },
+        {
+          rel: 'manifest',
+          href: '/manifest.json'
+        }
+      ]}
+    />
   )
 }
 
