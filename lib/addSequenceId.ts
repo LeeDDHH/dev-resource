d583e88294e6734326a9f4442c5b18d8e69baa2a
kamo <niche3600@gmail.com>
2023-07-04 19:57:29 +0900

    fix: 先にデータを逆順にしてからidを付与する

--- lib/addSequenceId.ts
+++ lib/addSequenceId.ts
@@ -7,9 +7,11 @@ let newDBObj: { resource: SequenceResourceData[] } = { resource: [] };
 
 const data = JSON.parse(readFileSync(originDataJsonPath));
 
-const sequenceIdData: SequenceResourceData[] = data.resource.map((v: ResourceData, i: number) => ({ ...v, id: i }));
+const sequenceIdData: SequenceResourceData[] = data.resource
+  .reverse()
+  .map((v: ResourceData, i: number) => ({ ...v, id: i }));
 
-newDBObj.resource = sequenceIdData.reverse();
+newDBObj.resource = sequenceIdData;
 
 const newJsonData = JSON.stringify(newDBObj);
 
