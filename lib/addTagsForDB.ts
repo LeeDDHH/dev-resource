d96f65afdb36ccd79c6dec0ded7ee1f808d70a3a
kamo <niche3600@gmail.com>
2023-07-05 08:53:03 +0900

    feat: 特定のURLに対してタグを追加するように修正

--- lib/addTagsForDB.ts
+++ lib/addTagsForDB.ts
@@ -1,4 +1,4 @@
-import { originDataJsonPath, dbTmpJsonPath, dbTagsJsonPath } from '@/lib/Const';
+import { originDataJsonPath, dbTagsJsonPath, dbTmpJsonPath } from '@/lib/Const';
 import { jsonFileExchange, readFileSync } from '@/lib/utils';
 
 import { ResourceData, JsonData } from '@/types/data';
@@ -10,16 +10,40 @@ const data = JSON.parse(readFileSync(originDataJsonPath)) as JsonData;
 const uniqueTagData = JSON.parse(readFileSync(dbTagsJsonPath)) as string[];
 const exsitingUniqueTags: string[] = [...new Set(uniqueTagData)];
 
-const tagAddedResourceData: ResourceData[] = data.resource.map((v: ResourceData) => {
-  if (v.tag.length > 0) return v;
-  // タグ一覧
+const addSpecifiedTagFromURL = (tags: (string | undefined)[], resourceData: ResourceData): (string | undefined)[] => {
+  const targetUrl = resourceData.url;
+  const cloneTags = [...tags];
+  switch (true) {
+    case /qiita.com/.test(targetUrl):
+    case /zenn.dev/.test(targetUrl):
+    case /dev.classmethod.jp/.test(targetUrl):
+    case /ics.media/.test(targetUrl):
+      cloneTags.push('記事');
+      return cloneTags;
+
+    case /youtube.com/.test(targetUrl):
+      cloneTags.push('Youtube');
+      return cloneTags;
+
+    default:
+      return cloneTags;
+  }
+};
+
+const tagAddedResourceData: ResourceData[] = data.resource.map((resourceData: ResourceData) => {
+  if (resourceData.tag.length > 0) return resourceData;
+  // タグ一覧とresourceData.descriptionを比較して該当するキーワードと比較した結果を格納する
   const tags = exsitingUniqueTags.map((tag: string) => {
     const reg = new RegExp(`${tag.toLowerCase()}`);
-    return reg.test(v.description.toLowerCase()) ? tag : undefined;
+    return reg.test(resourceData.description.toLowerCase()) ? tag : undefined;
   });
-  const uniqueTags = tags.filter((item): item is string => typeof item == 'string');
-  v.tag = uniqueTags;
-  return v;
+  // データのURLが特定のURLであれば、タグを追加する
+  const addedtags = addSpecifiedTagFromURL(tags, resourceData);
+  const concatTags = [...resourceData.tag, ...addedtags];
+  const stringTags = concatTags.filter((item): item is string => typeof item == 'string');
+  const uniqueTags = [...new Set(stringTags)];
+  resourceData.tag = uniqueTags;
+  return resourceData;
 });
 
 newDBObj.resource = tagAddedResourceData;
