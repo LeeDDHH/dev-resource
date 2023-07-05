aa3981b429a48e96f00001e948792f73df7ee977
kamo <niche3600@gmail.com>
2023-07-06 07:39:54 +0900

    chore: プロパティ名を修正

--- lib/tagCountPerResourceData.ts
+++ lib/tagCountPerResourceData.ts
@@ -6,7 +6,7 @@ import { JsonData } from '@/types/data';
 type TagNameCountData = { [key: string]: number };
 type TagCountData = {
   tag: string;
-  value: number;
+  count: number;
 };
 
 const data = JSON.parse(readFileSync(originDataJsonPath)) as JsonData;
@@ -21,13 +21,13 @@ const tagCountFromOriginData = tagList.reduce((acc: TagNameCountData, tagName: s
 
 const tagCountDataList: TagCountData[] = Object.keys(tagCountFromOriginData).map((tagName) => ({
   tag: tagName,
-  value: tagCountFromOriginData[tagName],
+  count: tagCountFromOriginData[tagName],
 }));
 
 // カウントされた数が多い順に並び替える
 tagCountDataList.sort((a: TagCountData, b: TagCountData) => {
-  if (a.value < b.value) return 1;
-  if (a.value > b.value) return -1;
+  if (a.count < b.count) return 1;
+  if (a.count > b.count) return -1;
   return 0;
 });
 
