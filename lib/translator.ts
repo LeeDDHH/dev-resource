defa63ccf59d2a0448160fa9ae70ccbb4c81853a
kamo <niche3600@gmail.com>
2023-07-09 13:42:07 +0900

    feat: google翻訳に対してAPIを送る関数を追加

---
+++ lib/translator.ts
@@ -0,0 +1,34 @@
+import Nightmare from 'nightmare';
+
+export const translator = async (text: string, to: string, from: string = 'auto'): Promise<string | undefined> => {
+  try {
+    const nightmare = new Nightmare({
+      ignoreDownloads: true,
+      waitTimeout: 60000,
+    });
+
+    const url = `https://translate.google.com/?sl=${from}&text=${text}&tl=${to}&op=translate`;
+    const translatedText = await nightmare
+      .goto(url)
+      .evaluate(() => {
+        // click ok on google consent
+        const button = document.querySelector('form[action="https://consent.google.com/s"] button') as HTMLElement;
+        if (button) {
+          button.click();
+        }
+      })
+      .wait('span[jsname=jqKxS]')
+      .wait(500)
+      .evaluate(() => (document.querySelector('span[jsname=jqKxS]') as HTMLElement).innerText)
+      .end();
+
+    if (!translatedText) {
+      throw new Error('Unable to translate.');
+    }
+
+    return translatedText.toString();
+  } catch (error) {
+    console.error(error);
+    // throw new Error(error);
+  }
+};
