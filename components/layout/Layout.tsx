5df7979219a3822d63256c62332aa2cfe2c8fbb2
kamo <niche3600@gmail.com>
2023-07-12 08:51:54 +0900

    fix: リソース数の表示方法を修正

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -8,12 +8,13 @@ import { MobileHamburgerMenu } from '@/components/parts/stateless/MobileHamburge
 
 import useMediaQuery from '@/hooks/useMediaQuery';
 
+import { itemsAmount } from '@/lib/Const';
+
 type Props = {
-  itemsAmount: number;
   children: React.ReactNode;
 };
 
-const Layout = React.memo(({ itemsAmount, children }: Props) => {
+const Layout = React.memo(({ children }: Props) => {
   const [openMenu, setOpenMenu] = useState(false);
   const isMaxWidthMd = useMediaQuery('(max-width: 767px)');
 
--- lib/Const.ts
+++ lib/Const.ts
@@ -2,6 +2,8 @@
 
 import path from 'path';
 
+import db from '@/data/db.json';
+
 export const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
 export const NODE_ENV = process.env.NODE_ENV;
 
@@ -16,3 +18,5 @@ export const dbTagsJsonPath = path.resolve(dataPath, 'db_tags.json');
 export const tagCountDataJsonPath = path.resolve(dataPath, 'db_tagCount.json');
 
 export const searchLimit = 12;
+
+export const itemsAmount = db.resource.length;
