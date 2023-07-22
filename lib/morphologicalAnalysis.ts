17b69ea61f28d989d32e7154c06a04bbe0db68b9
kamo <niche3600@gmail.com>
2023-07-22 12:11:53 +0900

    fix: mecabライブラリの指定の修正

--- lib/morphologicalAnalysis.ts
+++ lib/morphologicalAnalysis.ts
@@ -3,7 +3,10 @@ import { builder, IpadicFeatures, Tokenizer, TokenizerBuilder } from 'kuromoji';
 /**
  * @description 辞書データを読み込んだ日本語形態素解析モジュールを返す
  */
-const defaultBuilder: TokenizerBuilder<IpadicFeatures> = builder({ dicPath: 'lib/mecab-dict' });
+// lib/mecab-dict
+const defaultBuilder: TokenizerBuilder<IpadicFeatures> = builder({
+  dicPath: 'node_modules/kuromoji/dict',
+});
 
 /**
  * @description 文章を単語（＝トークン）に分解する非同期関数を返す
